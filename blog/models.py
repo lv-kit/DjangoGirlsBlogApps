638e1a1b49290a387fc146cd6b3eecadd4c2ce89
高橋岳宏 <taka.mc.0509@gmail.com>
2017-07-06 18:23:14 +0900

    綴の修正

--- blog/models.py
+++ blog/models.py
@@ -7,13 +7,11 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_data = models.DateTimeField(blank=True, null=True)
-
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_data = timezone.now()
         self.save()
-
 
     def __str__(self):
         return self.title
